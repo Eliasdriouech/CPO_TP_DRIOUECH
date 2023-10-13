af6a33d16f079af409c45d3ee3c06b72e1b91843
elias driouech <elias.driouech@epfedu.fr>
2023-10-13 11:54:13 +0200

    Création d'un nouveau projet versionné

--- TP0_DRIOUECH/TP2_manip_DRIOUECH/src/tp2_manip_driouech/TP2_manip_DRIOUECH.java
+++ TP0_DRIOUECH/TP2_manip_DRIOUECH/src/tp2_manip_driouech/TP2_manip_DRIOUECH.java
@@ -3,7 +3,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package tp2_manip_driouech;
+
 import java.util.Scanner;
+
 /**
  *
  * @author dreli
@@ -15,18 +17,28 @@ public class TP2_manip_DRIOUECH {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    int tab[]=new moussaka[10];
-    Tartiflette assiette1 = new Tartiflette(500);
-    Tartiflette assiette2 = new Tartiflette(600);
-    Tartiflette assiette3 = assiette2;
-    assiette2.nbCalories = assiette1.nbCalories;
-   assiette1.nbCalories= assiette3.nbCalories;
+        Moussaka[] moussakaReferences = new Moussaka[10];
+        Tartiflette assiette1 = new Tartiflette(500);
+        Tartiflette assiette2 = new Tartiflette(600);
+        Tartiflette assiette3 = assiette2;
+        assiette2.nbCalories = assiette1.nbCalories;
+        assiette1.nbCalories = assiette3.nbCalories;
 
-    System.out.println ("nb de calories de Assiette 2 : " +assiette2.nbCalories) ;
-    System.out.println ("nb de calories de Assiette 3 : " + assiette3.nbCalories) ;
-    for(int i=0;i<10;i++){
-        tab[i]=new Moussaka(500);
+        System.out.println("nb de calories de Assiette 2 : " + assiette2.nbCalories);
+        System.out.println("nb de calories de Assiette 3 : " + assiette3.nbCalories);
+
+        for (int i = 0; i < 10; i++) {
+
+            moussakaReferences[i] = new Moussaka((i + 1) * 100); // Utiliser une valeur arbitraire pour les calories
+
+        }
+
+        for (int i = 0; i < 10; i++) {
+
+            System.out.println("Référence " + (i + 1) + ":");
+
+            System.out.println("Nombre de calories : " + moussakaReferences[i].nbCalories);
+
+        }
     }
-    }
-    
 }
---
+++ TP0_DRIOUECH/TP2_manip_DRIOUECH/src/tp2_manip_driouech/Tartiflette.java
@@ -0,0 +1,17 @@
+package tp2_manip_driouech;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author dreli
+ */
+public class Tartiflette {
+    int nbCalories;
+    public Tartiflette(int unnbCalories) {
+    nbCalories=unnbCalories;
+    }
+}
